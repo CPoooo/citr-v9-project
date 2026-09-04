0b03b9c82cea8c4e2a71ccfa1bb7d683d70a7fde
cpool <cpool@kbsi.com>
2026-09-04 08:08:50 -0500

    eslint configured

--- papa-camerons/eslint.config.mjs
+++ papa-camerons/eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from "@eslint/js"
 import globals from "globals"
 import prettier from "eslint-config-prettier"
+import reactPlugin from "eslint-plugin-react"
 
 // type completions (if i had to guess this is just using typescript
 // in the js world?) so I have type completions and specifically these
@@ -9,7 +10,15 @@ import prettier from "eslint-config-prettier"
 // what would i do in the regular typescript world as well?  
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    js.configs.recommended,
+    js.configs.recommended, {
+        ...reactPlugin.configs.flat.recommended,
+        settings: {
+            react: {
+                version: 'detect'
+            },
+        },
+    },
+    reactPlugin.configs.flat["jsx-runtime"], // es-lint would be confused why we did not import React in a file that needs it without this 
     {
         files: ["**/*.js", "**/*.jsx"],
         languageOptions: {
@@ -21,6 +30,10 @@ export default [
                 }
             }
         },
+        rules: {
+            "react/no-unescaped-entities": "off", // cant put single quotes in strings '' -> &apos (who would do that)
+            "react/prop-types": "off",
+        }
     },
     prettier,
 ]
